bb26797276caf2509736ab4b73d679c275da4965
Mikel Díez Buil <mikeldiezbuil@gmail.com>
2018-06-01 21:47:23 +0200

    calibration completed

--- calibratecameras.py
+++ calibratecameras.py
@@ -7,9 +7,16 @@ if __name__ == '__main__':
     #Stop criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+    # Pattern Size
+    pattern_size = (8,6)
+
+    # Images format
+    images_format = '.png'
+
+
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6*8,3), np.float32)
-    objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)  
+    objp = np.zeros((pattern_size[1]*pattern_size[0],3), np.float32)
+    objp[:,:2] = np.mgrid[0:pattern_size[0],0:pattern_size[1]].T.reshape(-1,2)  
 
     # Arrays to store object points and image points from all the images.
     objpoints = [] # 3d point in real world space
@@ -17,8 +24,13 @@ if __name__ == '__main__':
     imgpoints_right = [] # 2d points in image plane.
     image_size = []
 
-    images_left = glob.glob('Calibration/images/left*.png')
-    images_right = glob.glob('Calibration/images/right*.png')
+    images_left = sorted(glob.glob('Calibration/images/set4/left*' + images_format))
+    images_right = sorted(glob.glob('Calibration/images/set4/right*' + images_format))
+
+    print images_left
+    print images_right
+
+
     for index,fname in enumerate(images_left):
 
         # Load images and convert to gray
@@ -28,9 +40,10 @@ if __name__ == '__main__':
         gray_right = cv2.cvtColor(img_right,cv2.COLOR_BGR2GRAY)
 
         image_size = img_left.shape
+
         # Find the chess board corners in both images
-        left_found, left_corners = cv2.findChessboardCorners(gray_left, (8,6),cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE | cv2.CALIB_CB_FAST_CHECK)
-        right_found, right_corners = cv2.findChessboardCorners(gray_right, (8,6),cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE | cv2.CALIB_CB_FAST_CHECK)
+        left_found, left_corners = cv2.findChessboardCorners(gray_left, pattern_size,cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE | cv2.CALIB_CB_FAST_CHECK)
+        right_found, right_corners = cv2.findChessboardCorners(gray_right, pattern_size,cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE | cv2.CALIB_CB_FAST_CHECK)
 
         if left_found:
             cv2.cornerSubPix(gray_left,left_corners,(11,11),(-1,-1),criteria)
@@ -45,10 +58,10 @@ if __name__ == '__main__':
             imgpoints_right.append(right_corners)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(img_left, (8,6), left_corners,left_found)
-        cv2.drawChessboardCorners(img_right, (8,6), right_corners,right_found)
+        cv2.drawChessboardCorners(img_left, pattern_size, left_corners,left_found)
+        cv2.drawChessboardCorners(img_right, pattern_size, right_corners,right_found)
         cv2.imshow('Left Image',img_left)
-        #cv2.waitKey(500)
+        cv2.waitKey(500)
 
     # Destroy windows
     cv2.destroyAllWindows()
@@ -58,16 +71,28 @@ if __name__ == '__main__':
     ret1, cameraMatrix1, distCoeffs1, rvecs1, tvecs1 = cv2.calibrateCamera(objpoints, imgpoints_left, gray_left.shape[::-1],None,None)
     ret2, cameraMatrix2, distCoeffs2, rvecs2, tvecs2 = cv2.calibrateCamera(objpoints, imgpoints_right, gray_right.shape[::-1],None,None)
 
+
+    # print cameraMatrix1 
+    # print distCoeffs1
+    # print cameraMatrix2 
+    # print distCoeffs2
+
+    # print 'objpoints'
+    # print objpoints
+    # print 'imgpoints_left'
+    # print imgpoints_left
+    # print 'imgpoints_right'
+    # print imgpoints_right
+
     # Calibrate stereo camera
     stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 1e-5)
-    stereocalib_flags = cv2.CALIB_FIX_ASPECT_RATIO | cv2.CALIB_ZERO_TANGENT_DIST | cv2.CALIB_SAME_FOCAL_LENGTH | cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_FIX_K3 | cv2.CALIB_FIX_K4 | cv2.CALIB_FIX_K5
+    # stereocalib_flags = cv2.CALIB_FIX_ASPECT_RATIO | cv2.CALIB_ZERO_TANGENT_DIST | cv2.CALIB_SAME_FOCAL_LENGTH | cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_FIX_K3 | cv2.CALIB_FIX_K4 | cv2.CALIB_FIX_K5
 
     stereocalib_flags = cv2.CALIB_FIX_INTRINSIC
     stereocalib_retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(objpoints,imgpoints_left,imgpoints_right,cameraMatrix1,distCoeffs1,cameraMatrix2,distCoeffs2,gray_left.shape[::-1],criteria = stereocalib_criteria, flags = stereocalib_flags)
 
 
-    # Calibrate camera
-    #ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray_left.shape[::-1],None,None)
+   
     
     # Save the calibration matrix in a yaml file.
     with open('calibrated_camera.yml', 'w') as outfile:
--- test_calibrated_cameras.py
+++ test_calibrated_cameras.py
@@ -3,43 +3,41 @@ import os
 import numpy as np
 import cv2
 
-
+def imShowTwoImages(image1,image2):
+    image = np.concatenate((image1, image2), axis=1)
+    color = 0
+    for index,line in enumerate(range(image1.shape[0]/10)):
+        
+        if(color > 255):
+            color = 0
+        cv2.line(image,(0,line*10),(1279,line*10),(color),1)
+        color += 50
+    cv2.imshow("rectified images", image)
 
 if __name__ == '__main__':
     with open("calibrated_camera.yml", 'r') as stream:
         try:
             data = yaml.load(stream) 
 
-            rectify_scale = 0 # 0=full crop, 1=no crop
+            print data["R"]
+            print data["T"]
+
+            rectify_scale = 1 # 0=full crop, 1=no crop
             R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(data["cameraMatrix1"], data["distCoeffs1"], data["cameraMatrix2"], data["distCoeffs2"], (640, 480), data["R"], data["T"], alpha = rectify_scale)
-
-            print roi2
-            print R1
-            print R2
-            print Q
-
 
             left_maps = cv2.initUndistortRectifyMap(data["cameraMatrix1"], data["distCoeffs1"], R1, P1, (640, 480), cv2.CV_16SC2)
             right_maps = cv2.initUndistortRectifyMap(data["cameraMatrix2"], data["distCoeffs2"], R2, P2, (640, 480), cv2.CV_16SC2)
 
 
-            img_left = cv2.imread('left_image_3.png')
+            img_left = cv2.imread('Calibration/images/set3/left_image_5.png')
             gray_left = cv2.cvtColor(img_left,cv2.COLOR_BGR2GRAY)
-            img_right = cv2.imread('right_image_3.png')
+            img_right = cv2.imread('Calibration/images/set3/right_image_5.png')
             gray_right = cv2.cvtColor(img_right,cv2.COLOR_BGR2GRAY)
              
             left_img_remap = cv2.remap(gray_left, left_maps[0], left_maps[1], cv2.INTER_LANCZOS4)
             right_img_remap = cv2.remap(gray_right, right_maps[0], right_maps[1], cv2.INTER_LANCZOS4)
 
-            print type(gray_right)
-            print gray_right.shape
-            print gray_right
-            print type(right_img_remap)
-            print right_img_remap.shape
-            print right_img_remap
-
-            cv2.imshow("image 1", left_img_remap)
-            cv2.imshow("image 2", right_img_remap)
+            imShowTwoImages(left_img_remap,right_img_remap)
             cv2.waitKey(0)
 
         except yaml.YAMLError as exc:
