0ad42e15b1bb7597171eda65876e96a6e462f8b3
Mikel Díez Buil <mikeldiezbuil@gmail.com>
2019-02-18 00:03:48 +0100

    New things

--- Applications/3DReconstruction/stereoreconstruction.py
+++ Applications/3DReconstruction/stereoreconstruction.py
@@ -5,60 +5,49 @@ import yaml
 import os
 
 from Modules.Matching.imagematcher import ClassicMatcher
+from Modules.GUI.Helpers import imShowTwoImages, drawlines
 
-def imShowTwoImages(image1,image2):
-    image = np.concatenate((image1, image2), axis=1)
-    color = 0
-    for index,line in enumerate(range(image1.shape[0]/10)):
-        
-        if(color > 255):
-            color = 0
-        cv2.line(image,(0,line*10),(1279,line*10),(color),1)
-        color += 50
-    cv2.imshow("rectified images", image)
-
-
-# Here goes the script
 if __name__ == '__main__':
 	# Take the images from folder/camera and rectify
-	with open("bin/CalibrationMatrix/calibrated_camera.yml", 'r') as stream:
+	with open("bin/CalibrationMatrix/set11/calibrated_camera.yml", 'r') as stream:
 		try:
 			data = yaml.load(stream)
 
-			print data['F']
-			print data['T']
-			print data['F'] * data['T']
+			print(data.keys())
+
+			print('R', data['R'])
+			print('T', data['T'])
+			print('F', data['F'])
+			print('cameraMatrix1', data['cameraMatrix1'])
+			print('cameraMatrix2', data['cameraMatrix2'])
 
 			rectify_scale = 0 # 0=full crop, 1=no crop
 			R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(data["cameraMatrix1"], data["distCoeffs1"], data["cameraMatrix2"], data["distCoeffs2"], (640, 480), data["R"], data["T"], alpha = rectify_scale)
 
-			left_maps = cv2.initUndistortRectifyMap(data["cameraMatrix1"], data["distCoeffs1"], R1, P1, (640, 480), cv2.CV_16SC2)
-			right_maps = cv2.initUndistortRectifyMap(data["cameraMatrix2"], data["distCoeffs2"], R2, P2, (640, 480), cv2.CV_16SC2)
+			left_maps = cv2.initUndistortRectifyMap(data["cameraMatrix1"], data["distCoeffs1"], R1, P1, (1920, 1080), cv2.CV_16SC2)
+			right_maps = cv2.initUndistortRectifyMap(data["cameraMatrix2"], data["distCoeffs2"], R2, P2, (1920, 1080), cv2.CV_16SC2)
 
-			img_left = cv2.imread('Images/left_image_2.png')
-			img_right = cv2.imread('Images/right_image_2.png')
+			img_left = cv2.imread('bin/CalibrationImages/set11_objectReconstruction/left_image_3.png')
+			img_right = cv2.imread('bin/CalibrationImages/set11_objectReconstruction/right_image_3.png')
 
-			gray_left = cv2.cvtColor(img_left,cv2.COLOR_BGR2GRAY)
-			gray_right = cv2.cvtColor(img_right,cv2.COLOR_BGR2GRAY)
+			imShowTwoImages(img_left, img_right, 'original imgs')
+			cv2.waitKey(0)
+
+			gray_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+			gray_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
 
 			left_img_remap = cv2.remap(gray_left, left_maps[0], left_maps[1], cv2.INTER_LANCZOS4)
 			right_img_remap = cv2.remap(gray_right, right_maps[0], right_maps[1], cv2.INTER_LANCZOS4)
 
-			imShowTwoImages(left_img_remap,right_img_remap)
+			imShowTwoImages(gray_left, gray_right, 'original grey')
 			cv2.waitKey(0)
+
+			imShowTwoImages(left_img_remap,right_img_remap, 'remapped images')
+			cv2.waitKey(0)
+
+			lines1 = cv2.computeCorrespondEpilines((0, 0, 0), 1, data['F'])
+			lines1 = lines1.reshape(-1, 3)
+			left_img_lines, right_img_lines = drawlines(left_img_remap, right_img_remap, lines1, pts1, pts2)
 
 		except yaml.YAMLError as exc:
 			print(exc)
-
-	# Find matching points in both cameras
-	matcher = ClassicMatcher()
-	matcher.setImages(left_img_remap,right_img_remap)
-	matcher.setPointsOfInterest()
-	images = matcher.getPointsOfInterest()
-	imShowTwoImages(images[0],images[1])
-	cv2.waitKey(0)
-	print matcher.matchPoints()
-	matcher.showImages()
-	# Get the 3d space points from 2d pixel pairs
-
-	# Print the points in some kind of graphic interface
---
+++ Modules/GUI/Helpers.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+
+def drawlines(img1, img2, lines, pts1, pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    r, c = img1.shape
+    img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+        x0, y0 = map(int, [0, -r[2] / r[1]])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
+        img1 = cv2.line(img1, (x0, y0), (x1, y1), color, 1)
+        img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
+        img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
+    return img1, img2
+
+
+def imShowTwoImages(image1, image2, title):
+    image = np.concatenate((cv2.resize(image1, (640, 480)), cv2.resize(image2, (640, 480))), axis=1)
+    color = 0
+    for index, line in enumerate(range(image1.shape[0] / 10)):
+
+        if (color > 255):
+            color = 0
+        cv2.line(image, (0, line * 10), (1279, line * 10), (color), 1)
+        color += 50
+    cv2.imshow(title, image)
