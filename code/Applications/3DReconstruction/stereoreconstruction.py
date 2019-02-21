2f644573a6c4501f4cf2a06b73d8875af258d075
Mikel Díez Buil <mikeldiezbuil@gmail.com>
2019-02-21 21:54:56 +0100

    First reconstruction

--- code/Applications/3DReconstruction/stereoreconstruction.py
+++ code/Applications/3DReconstruction/stereoreconstruction.py
@@ -46,6 +46,7 @@ if __name__ == '__main__':
 			# cv2.waitKey(0)
 
 			points = np.array([[[858, 271]], [[897, 453]], [[1090, 276]], [[1123, 444]]], dtype=np.float32)
+			points2 = np.array([[[709, 255]], [[733, 432]], [[949, 265]], [[969, 430]]], dtype=np.float32)
 
 			lines1 = cv2.computeCorrespondEpilines(points, 1, data['F'])
 			lines1 = lines1.reshape(-1, 3)
@@ -54,5 +55,11 @@ if __name__ == '__main__':
 
 			imShowTwoImages(left_img_lines, right_img_lines, 'wiiii')
 			cv2.waitKey(0)
+
+			points4D = cv2.triangulatePoints(P1, P2, points, points2)
+			print(points4D)
+			newArray = [np.array([-0.12184567, -0.14085074, -0.23511265]) / -0.24977995, np.array([ 0.00702684, -0.08393233,  0.00313828]) / -0.07938664, np.array([-0.99220562, -0.98608309, -0.97168368]) / -0.96471202, np.array([-0.02514401, -0.02750583, -0.0233017]) / -0.02526825]
+			print(newArray)
+
 		except yaml.YAMLError as exc:
 			print(exc)
